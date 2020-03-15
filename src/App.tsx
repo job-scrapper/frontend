87a3217a6635838c9d659cefd3cd5251da0cdd91
huskyhoochu <dfg1499@gmail.com>
2020-03-15 18:28:18 +0900

    Install React-Router-Dom & Create router
    - 페이지를 분할하기 위해 react-router-dom 설치
    - home page 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import Header from './components/header';
+import Routes from './router/routes';
 
 function App(): JSX.Element {
   return (
     <div className="App" data-testid="app">
       <Header />
+      <Routes />
     </div>
   );
 }
