44c5fe58b3bff9406350f2ce6bdd2fbc1cf8b0d5
huskyhoochu <dfg1499@gmail.com>
2020-03-22 17:52:21 +0900

    Add test router

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { StaticRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByTestId } = render(<App />);
+  const { getByTestId } = render(
+    <StaticRouter>
+      <App />
+    </StaticRouter>,
+  );
   const linkElement = getByTestId('app');
   expect(linkElement).toBeInTheDocument();
 });
