07d42ca05b806d1cd5488193de539d3cea6af0bc
huskyhoochu <dfg1499@gmail.com>
2020-03-22 19:49:50 +0900

    Add Menus left side
    - 좌측에 메뉴 펼치는 아이콘 생성하고 메뉴 만들어냄

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react';
-import { Navbar, Alignment, Button, Colors } from '@blueprintjs/core';
+import React, { useContext, useState } from 'react';
+import { Navbar, Alignment, Button, Colors, Drawer, Position, Classes } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
 import { Link } from 'react-router-dom';
 
 import { Auth0Context } from '../../router/auth0Context';
@@ -7,6 +8,7 @@ import { Flexbox, Email, Profile } from './styled';
 
 function Header(): JSX.Element {
   const { auth0Client, user, isAuthenticated } = useContext(Auth0Context);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   function logout(): void {
     if (auth0Client) {
@@ -15,11 +17,34 @@ function Header(): JSX.Element {
     }
   }
 
+  function toggleDrawer(): void {
+    setIsOpen(state => !state);
+  }
+
   return (
     <Navbar data-testid="header" className="bp3-dark">
       <Navbar.Group align={Alignment.LEFT}>
         <Navbar.Heading>
-          <Link to="/" style={{ color: Colors.WHITE, textDecoration: 'none' }}>
+          <Button icon={IconNames.MENU} onClick={toggleDrawer} />
+          <Drawer
+            isOpen={isOpen}
+            onClose={toggleDrawer}
+            position={Position.LEFT}
+            size={200}
+            title="Menu"
+            canOutsideClickClose
+          >
+            <div className={Classes.DRAWER_BODY}>
+              <ul className={Classes.LIST}>
+                <li>
+                  <Link to="/scrapper/list" onClick={toggleDrawer}>
+                    스크래핑 목록
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </Drawer>
+          <Link to="/" style={{ color: Colors.WHITE, textDecoration: 'none', margin: '0 16px' }}>
             Scrapper
           </Link>
         </Navbar.Heading>
